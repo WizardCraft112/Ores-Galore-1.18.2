01c1697b0b50e9cd8c30385772002686ffb429b3
Pranay Bokde <pranay.db21@gmail.com>
2022-04-16 11:55:09 -0700

    added item registry

--- src/main/java/net/wizardcraft112/oresgalore/OresGalore.java
+++ src/main/java/net/wizardcraft112/oresgalore/OresGalore.java
@@ -1,15 +1,18 @@
 package net.wizardcraft112.oresgalore;
 
 import net.fabricmc.api.ModInitializer;
+import net.wizardcraft112.oresgalore.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OresGalore implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "oresgalore";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/wizardcraft112/oresgalore/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.wizardcraft112.oresgalore.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.wizardcraft112.oresgalore.OresGalore;
+
+
+public class ModItems {
+    public static final Item ENDIUM_INGOT = registerItem("endium_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(OresGalore.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        OresGalore.LOGGER.info("Registering Mod Items for " + OresGalore.MOD_ID);
+    }
+
+}
