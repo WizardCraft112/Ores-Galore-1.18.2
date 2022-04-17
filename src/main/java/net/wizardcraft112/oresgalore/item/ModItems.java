200d81e64a481551675cd3594de623df681402eb
Pranay Bokde <pranay.db21@gmail.com>
2022-04-17 09:14:11 -0700

    added mythril nugget

--- src/main/java/net/wizardcraft112/oresgalore/item/ModItems.java
+++ src/main/java/net/wizardcraft112/oresgalore/item/ModItems.java
@@ -15,6 +15,9 @@ public class ModItems {
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
             new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
 
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(OresGalore.MOD_ID, name), item);
     }
